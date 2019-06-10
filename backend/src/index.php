def81be972cab880fb4c5e3b6cda42f653eb327f
Mark Robertshaw <mark@oxil.co.uk>
2019-06-10 17:36:37 +0100

    New example logic.

---
+++ backend/src/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Kinikit\MVC\Framework\Dispatcher;
+
+// Include the vendor autoload.
+include_once "../../vendor/autoload.php";
+
+// Dispatch the Kinikit dispatcher.
+(new Dispatcher())->dispatch();
+
+
