849b88cc94878fc8102226a5f2084fedb6cae2af
Nathan Alan <nathan@oxil.co.uk>
2019-06-13 15:59:02 +0100

    Angular 8 fixes

--- dashboard/admin/src/app/app.module.ts
+++ dashboard/admin/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgKinicartModule } from 'ng-kinicart';
 import { AccountSummaryComponent } from './account-summary/account-summary.component';
